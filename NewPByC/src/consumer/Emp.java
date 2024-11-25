9832a4365ad5dc81b46f38e4cf8642823ce26780
sdac-21 <sdac-21@sdac-21-PC>
2024-11-25 12:29:04 +0530

    consumer

---
+++ NewPByC/src/consumer/Emp.java
@@ -0,0 +1,13 @@
+package consumer;
+
+import provider.CompIml;
+
+public class Emp extends CompIml {
+
+	@Override
+	public double calSal(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
